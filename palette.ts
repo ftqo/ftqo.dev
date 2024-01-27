16ad27dacd1e015ec9ea948b47b3c61d545670c1
ftqo <brian@ftqo.dev>
2024-01-27 18:41:02 -0500

    convert to flexoki, custom tailwind plugin

---
+++ palette.ts
@@ -0,0 +1,281 @@
+const flexoki_light = {
+  re: {
+    hex: '#AF3029',
+  },
+  re_2: {
+    hex: '#D14D41',
+  },
+
+  or: {
+    hex: '#BC5215',
+  },
+  or_2: {
+    hex: '#DA702C',
+  },
+
+  ye: {
+    hex: '#AD8301',
+  },
+  ye_2: {
+    hex: '#D0A215',
+  },
+
+  gr: {
+    hex: '#66800B',
+  },
+  gr_2: {
+    hex: '#879A39',
+  },
+
+  cy: {
+    hex: '#24837B',
+  },
+  cy_2: {
+    hex: '#3AA99F',
+  },
+
+  bl: {
+    hex: '#205EA6',
+  },
+  bl_2: {
+    hex: '#4385BE',
+  },
+
+  pu: {
+    hex: '#5E409D',
+  },
+  pu_2: {
+    hex: '#8B7EC8',
+  },
+
+  ma: {
+    hex: '#A02F6F',
+  },
+  ma_2: {
+    hex: '#CE5D97',
+  },
+
+  bg: {
+    hex: '#FFFCF0',
+  },
+  bg_2: {
+    hex: '#F2F0E5',
+  },
+
+  tx: {
+    hex: '#100F0F',
+  },
+  tx_2: {
+    hex: '#6F6E69',
+  },
+  tx_3: {
+    hex: '#B7B5AC',
+  },
+
+  ui: {
+    hex: '#E6E4D9',
+  },
+  ui_2: {
+    hex: '#DAD8CE',
+  },
+  ui_3: {
+    hex: '#CECDC3',
+  },
+}
+
+const flexoki = {
+  re: {
+    hex: '#D14D41',
+  },
+  re_2: {
+    hex: '#AF3029',
+  },
+
+  or: {
+    hex: '#DA702C',
+  },
+  or_2: {
+    hex: '#BC5215',
+  },
+
+  ye: {
+    hex: '#D0A215',
+  },
+  ye_2: {
+    hex: '#AD8301',
+  },
+
+  gr: {
+    hex: '#879A39',
+  },
+  gr_2: {
+    hex: '#66800B',
+  },
+
+  cy: {
+    hex: '#3AA99F',
+  },
+  cy_2: {
+    hex: '#24837B',
+  },
+
+  bl: {
+    hex: '#4385BE',
+  },
+  bl_2: {
+    hex: '#205EA6',
+  },
+
+  pu: {
+    hex: '#8B7EC8',
+  },
+  pu_2: {
+    hex: '#5E409D',
+  },
+
+  ma: {
+    hex: '#CE5D97',
+  },
+  ma_2: {
+    hex: '#A02F6F',
+  },
+
+  bg: {
+    hex: '#100F0F',
+  },
+  bg_2: {
+    hex: '#1C1B1A',
+  },
+
+  tx: {
+    hex: '#CECDC3',
+  },
+  tx_2: {
+    hex: '#878580',
+  },
+  tx_3: {
+    hex: '#575653',
+  },
+
+  ui: {
+    hex: '#282726',
+  },
+  ui_2: {
+    hex: '#343331',
+  },
+  ui_3: {
+    hex: '#403E3C',
+  },
+}
+
+const palettes = {
+  variants: {
+    flexoki_light: flexoki_light,
+    flexoki: flexoki,
+  },
+  labels: {
+    re: {
+      flexoki_light: flexoki_light.re,
+      flexoki: flexoki.re,
+    },
+    re_2: {
+      flexoki_light: flexoki_light.re_2,
+      flexoki: flexoki.re_2,
+    },
+    or: {
+      flexoki_light: flexoki_light.or,
+      flexoki: flexoki.or,
+    },
+    or_2: {
+      flexoki_light: flexoki_light.or_2,
+      flexoki: flexoki.or_2,
+    },
+    ye: {
+      flexoki_light: flexoki_light.ye,
+      flexoki: flexoki.ye,
+    },
+    ye_2: {
+      flexoki_light: flexoki_light.ye_2,
+      flexoki: flexoki.ye_2,
+    },
+    gr: {
+      flexoki_light: flexoki_light.gr,
+      flexoki: flexoki.gr,
+    },
+    gr_2: {
+      flexoki_light: flexoki_light.gr_2,
+      flexoki: flexoki.gr_2,
+    },
+    cy: {
+      flexoki_light: flexoki_light.cy,
+      flexoki: flexoki.cy,
+    },
+    cy_2: {
+      flexoki_light: flexoki_light.cy_2,
+      flexoki: flexoki.cy_2,
+    },
+    bl: {
+      flexoki_light: flexoki_light.bl,
+      flexoki: flexoki.bl,
+    },
+    bl_2: {
+      flexoki_light: flexoki_light.bl_2,
+      flexoki: flexoki.bl_2,
+    },
+    pu: {
+      flexoki_light: flexoki_light.pu,
+      flexoki: flexoki.pu,
+    },
+    pu_2: {
+      flexoki_light: flexoki_light.pu_2,
+      flexoki: flexoki.pu_2,
+    },
+    ma: {
+      flexoki_light: flexoki_light.ma,
+      flexoki: flexoki.ma,
+    },
+    ma_2: {
+      flexoki_light: flexoki_light.ma_2,
+      flexoki: flexoki.ma_2,
+    },
+    bg: {
+      flexoki_light: flexoki_light.bg,
+      flexoki: flexoki.bg,
+    },
+    bg_2: {
+      flexoki_light: flexoki_light.bg_2,
+      flexoki: flexoki.bg_2,
+    },
+    tx: {
+      flexoki_light: flexoki_light.tx,
+      flexoki: flexoki.tx,
+    },
+    tx_2: {
+      flexoki_light: flexoki_light.tx_2,
+      flexoki: flexoki.tx_2,
+    },
+    tx_3: {
+      flexoki_light: flexoki_light.tx_3,
+      flexoki: flexoki.tx_3,
+    },
+    ui: {
+      flexoki_light: flexoki_light.ui,
+      flexoki: flexoki.ui,
+    },
+    ui_2: {
+      flexoki_light: flexoki_light.ui_2,
+      flexoki: flexoki.ui_2,
+    },
+    ui_3: {
+      flexoki_light: flexoki_light.ui_3,
+      flexoki: flexoki.ui_3,
+    },
+  }
+};
+const { variants, labels } = palettes;
+const palette_default = palettes;
+export {
+  palette_default as default,
+  labels,
+  variants
+};
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,16 +6,16 @@ module.exports = {
       spacing: {
         'content': '980px',
         'content-sm': '87.5%'
+      },
+      colors: {
+        
       }
     },
   },
   plugins: [
-    require("@catppuccin/tailwindcss")({
-      // prefix to use, e.g. `text-pink` becomes `text-ctp-pink`.
-      // default is `false`, which means no prefix
-      prefix: "ctp",
-      // which flavour of colours to use by default, in the `:root`
-      defaultFlavour: "latte",
+    require("./plugin")({
+      prefix: "flx",
+      defaultFlavour: "flexoki_light",
     }),
     require("tailwind-fluid-typography")
   ],
